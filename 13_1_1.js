07e50c05fcdcaba06e1cea28857ace550340d03f
xenocurrency <pressk0108@gmail.com>
2023-12-29 06:45:54 +0900

    e.target , event(change / input)

--- 13_1_1.js
+++ 13_1_1.js
@@ -1,20 +1,34 @@
 document.addEventListener("DOMContentLoaded",
     function(e){
-        document.addEventListener("keydown",
-            function(e){
-                console.log(e.key)
-                if (e.key == "q"){
-                    alert("this is Q!")
-                }
-            }
-        )
-        
-        
         let keydown = document.querySelector("#keydown")
         keydown.addEventListener("keydown",
             function(e){
                 console.log(e.target.value)
             }
         )
+
+        let change = document.querySelector("#change")
+        change.addEventListener("change",
+            function(e){
+                console.log(e.target.value)
+            }
+        )
+
+        let input = document.querySelector("#input")
+        input.addEventListener("input",
+            function(e){
+                console.log(e.target.value)
+            }
+        )
     }
 )
+
+function red(e){
+    console.log(e)
+    e.target.style.backgroundColor = "red"
+}
+
+function blue(e){
+    console.log(e)
+    e.target.style.backgroundColor = "blue"
+}
