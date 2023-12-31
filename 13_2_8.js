bb83a6daa27fda5c2959f2b75ba6f08d9b0132ab
xenocurrency <pressk0108@gmail.com>
2024-01-01 08:03:00 +0900

    nameEditV1

---
+++ 13_2_8.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded",
+    function(e){
+        let nameEditButton = document.querySelector("#nameEditButton")
+        let editNameField = document.querySelector("#editNameField")
+        let newName = document.querySelector("#newName")
+        let okButton = document.querySelector("#okButton")
+        let nameHere = document.querySelector("#nameHere")
+        
+        nameEditButton.addEventListener("click",
+            function(e){
+
+            }
+        )
+
+        okButton.addEventListener("click",
+        function(e){
+            
+        }
+    )
+    }
+)
