279817d4837a8f1e37edfc78a6a1a582029cd83a
xenocurrency <pressk0108@gmail.com>
2024-01-04 11:36:03 +0900

    edit HTML using JS

--- script.js
+++ script.js
@@ -8,5 +8,45 @@ document.addEventListener("DOMContentLoaded",
                 idText.textContent = prompt("새로운 아이디를 입력하세요.")
             }
         )
+
+        let profileEditButton = document.querySelector("#profile_info button")
+        let userInfo = document.querySelector("#userInfo")
+        let summary = document.querySelector("#summary")
+        let profileDetail = document.querySelector("#profileDetail")
+        let changing = false
+
+        profileEditButton.addEventListener("click",
+            function(e){
+                if (changing) {
+                    let _userInfo = userInfo.querySelector("input").value
+                    let _summary = summary.querySelector("input").value
+                    let _profileDetail = profileDetail.querySelector("input").value
+
+                    userInfo.innerHTML = _userInfo
+                    summary.innerHTML = _summary
+
+                    if (_profileDetail.startsWith("http")){
+                        _profileDetail = "<a href=" + _profileDetail + ">" + _profileDetail + "</a>"
+                        }
+
+                    profileDetail.innerHTML = _profileDetail
+
+                    e.target.textContent = "프로필 편집"
+                    changing = false
+
+                }else{
+                    let _userInfo = userInfo.textContent
+                    let _summary = summary.textContent
+                    let _profileDetail = profileDetail.textContent
+
+                    userInfo.innerHTML = "<input value=" + _userInfo + "></input>"
+                    summary.innerHTML = "<input value=" + _summary + "></input>"
+                    profileDetail.innerHTML = "<input value=" + _profileDetail + "></input>"
+
+                    e.target.textContent = "프로필 편집 완료"
+                    changing = true
+                }
+            }   
+        )
     }
 )
